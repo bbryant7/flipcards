e706ddfcf3ac0c779b6d642ce0d0a16ec21d859b
Bailey Bryant <bailey.bryant10@gmail.com>
2017-09-08 14:02:30 -0500

    login page works

--- models/userdata.js
+++ models/userdata.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userData = new mongoose.Schema({
+const userData = new mongoose.Schema({ 
         username: {type: String, required: true, unique: true},
         password: {type: String, required: true}
   })
